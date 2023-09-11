d564f51b315f232755924aaf238291af463e4134
misung.dev <misung.dev@gmail.com>
2023-09-11 23:43:21 +0900

    Feat: 홈페이지 InformationCard 개발

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,10 +8,43 @@ const HomePage = () => {
 				<S.Profile>
 					<S.ProfileImg src="/images/img-sample-profile-img.png" alt="프로필 이미지" />
 					<S.ProfileInfo>
-						<S.ProfileName>Sakshi</S.ProfileName>
+						<S.ProfileName>apps</S.ProfileName>
 						<S.ProfileEmail>sakshichoudhary@email.com</S.ProfileEmail>
 					</S.ProfileInfo>
 				</S.Profile>
+
+				<S.Calender>캘린더 영역</S.Calender>
+
+				<S.InformationCard>
+					<S.InformationList>
+						<S.InformationItem>
+							<S.InformationItemIcon src="/svgs/svg-calender.svg" alt="달력" />
+							<S.InformationItemContents>2023년 5월 11일 (목)</S.InformationItemContents>
+						</S.InformationItem>
+
+						<S.InformationItem>
+							<S.InformationItemIcon src="/svgs/svg-clock.svg" alt="시계" />
+							<S.InformationItemContents>18:00 pm</S.InformationItemContents>
+						</S.InformationItem>
+
+						<S.InformationItem>
+							<S.InformationItemIcon src="/svgs/svg-location.svg" alt="위치" />
+							<S.InformationItemContents>명신관 315</S.InformationItemContents>
+						</S.InformationItem>
+
+						<S.InformationItem>
+							<S.InformationItemIcon src="/svgs/svg-todo-list.svg" alt="할일" />
+							<S.InformationItemContentsList>
+								<S.InformationItemContentsItem>스터디 발표</S.InformationItemContentsItem>
+								<S.InformationItemContentsItem>프로젝트 발표</S.InformationItemContentsItem>
+								<S.InformationItemContentsItem>공지</S.InformationItemContentsItem>
+								<S.InformationItemContentsItem>DevTalk</S.InformationItemContentsItem>
+							</S.InformationItemContentsList>
+						</S.InformationItem>
+					</S.InformationList>
+
+					<S.AttendanceCheckButton>출석체크</S.AttendanceCheckButton>
+				</S.InformationCard>
 			</S.Container>
 		</PageLayout>
 	);
--- src/pages/HomePage/HomePage.style.jsx
+++ src/pages/HomePage/HomePage.style.jsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-	padding: 0 20px;
+	padding: 20px;
 `;
 
 export const Profile = styled.div`
@@ -31,4 +31,66 @@ export const ProfileName = styled.span`
 export const ProfileEmail = styled.span`
 	font-size: 13px;
 	font-weight: 600;
+`;
+
+export const Calender = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100px;
+	margin: 20px 0;
+	border: 1px solid black;
+`;
+
+export const InformationCard = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 30px;
+	width: 100%;
+	border-radius: 11px;
+	border: 1px solid #dedede;
+	padding: 18px;
+`;
+
+export const InformationList = styled.ul`
+	display: flex;
+	flex-direction: column;
+	gap: 22px;
+`;
+
+export const InformationItem = styled.li`
+	display: flex;
+	align-items: flex-start;
+	gap: 12px;
+	font-size: 16px;
+	color: #4e4949;
+`;
+
+export const InformationItemIcon = styled.img`
+	--size: 26px;
+	width: var(--size);
+	height: var(--size);
+`;
+
+export const InformationItemContents = styled.span`
+	margin-top: 5px;
+`;
+
+export const InformationItemContentsList = styled.ol`
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	margin-top: 5px;
+`;
+
+export const InformationItemContentsItem = styled.li``;
+
+export const AttendanceCheckButton = styled.button`
+	width: 100%;
+	height: 48px;
+	border-radius: 20px;
+	background: #2b80ff;
+	color: #fff;
+	font-size: 18px;
+	font-weight: 500;
 `;
