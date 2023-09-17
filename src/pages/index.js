01c235becba33c7556e10248f849813246c5e38a
misung.dev <misung.dev@gmail.com>
2023-09-17 18:00:40 +0900

    Feat: 출석 완료 페이지 제작

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,5 @@ export { default as HomePage } from "./HomePage/HomePage";
 export { default as LoginPage } from "./LoginPage";
 
 export { default as SignUpPage } from "./SignUpPage";
+
+export { default as AttendanceCompletionPage } from "./AttendanceCompletionPage";
