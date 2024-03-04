31ff58b07bc253389901b8af8a4a7ac2b56a356a
misung.dev <misung.dev@gmail.com>
2024-03-05 00:06:04 +0900

    Feat: 회원가입 페이지 일부 구현

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,6 +1,179 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./SignupPage.style";
 
 export default function SignupPage() {
-	return <div>SignupPage</div>;
+	const [email, setEmail] = useState(""); //빈 문자열로 초기화
+	const [pw, setPw] = useState("");
+
+	const [emailValid, setEmailValid] = useState(false);
+	const [pwValid, setPwValid] = useState(false);
+	const [notAllow, setNotAllow] = useState(true); // 확인버튼의 활성화와 비활성화
+
+	const handleEmail = (e) => {
+		const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // 이메일 정규식
+
+		setEmail(e.target.value);
+		setEmailValid(regex.test(email) ? true : false);
+		// if (regex.test(email)) {
+		// 	setEmailValid(true);
+		// } else {
+		// 	setEmailValid(false);
+		// }
+	};
+
+	const handlePassword = (e) => {
+		const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; // 비밀번호 정규식
+
+		setPw(e.target.value);
+		setPwValid(regex.test(pw) ? true : false);
+		// if (regex.test(pw)) {
+		// 	setPwValid(true);
+		// } else {
+		// 	setPwValid(false);
+		// }
+	};
+
+	useEffect(() => {
+		if (emailValid && pwValid) {
+			setNotAllow(false);
+			return;
+		}
+		setNotAllow(true);
+	}, [emailValid, pwValid]);
+
+	return (
+		<S.Container>
+			<S.SignupContainer>
+				<S.SignupTitle>회원 가입</S.SignupTitle>
+
+				<S.SignupContent>
+					<S.Divider>
+						<S.Content>
+							<S.ContentTitle>이름</S.ContentTitle>
+							<S.ContentInputWrap>
+								<S.ContentInput
+									type="text"
+									name="name"
+									id="name"
+									minlength="5"
+									placeholder="김앱스"
+								></S.ContentInput>
+							</S.ContentInputWrap>
+						</S.Content>
+
+						<S.Content>
+							<S.ContentTitle>회장 여부</S.ContentTitle>
+							<S.ContentInputWrap>
+								<S.ContentInput
+									type="text"
+									name="name"
+									id="name"
+									minlength="5"
+									placeholder="김앱스"
+								></S.ContentInput>
+							</S.ContentInputWrap>
+						</S.Content>
+					</S.Divider>
+
+					<S.Divider>
+						<S.Content>
+							<S.ContentTitle>전공</S.ContentTitle>
+							<S.ContentInputWrap>
+								<S.ContentInput
+									type="text"
+									name="name"
+									id="name"
+									placeholder="컴퓨터과학전공"
+								></S.ContentInput>
+							</S.ContentInputWrap>
+						</S.Content>
+
+						<S.Content>
+							<S.ContentTitle>학번</S.ContentTitle>
+							<S.ContentInputWrap>
+								<S.ContentInput
+									type="text"
+									name="name"
+									id="name"
+									minlength="2"
+									placeholder="21 / 22 / 23 / 24 ..."
+								></S.ContentInput>
+							</S.ContentInputWrap>
+						</S.Content>
+					</S.Divider>
+
+					<S.Content>
+						<S.ContentTitle>연락처</S.ContentTitle>
+						<S.ContentInputWrap>
+							<S.ContentInput
+								type="text"
+								name="name"
+								id="name"
+								placeholder="010-1234-5678"
+							></S.ContentInput>
+						</S.ContentInputWrap>
+					</S.Content>
+
+					<S.Content>
+						<S.ContentTitle>이메일</S.ContentTitle>
+						<S.ContentInputWrap>
+							<S.ContentInput
+								type="email"
+								name="email"
+								placeholder="test@gmail.com"
+								value={email}
+								onChange={handleEmail}
+							></S.ContentInput>
+							{/* <S.Check id="emailCheck"></S.Check> */}
+						</S.ContentInputWrap>
+						<S.ErrorMessageWrap>
+							{!emailValid && email.length > 0 && <div>올바른 이메일을 입력해주세요.</div>}
+						</S.ErrorMessageWrap>
+					</S.Content>
+
+					<S.Content>
+						<S.ContentTitle>비밀번호</S.ContentTitle>
+						<S.ContentInputWrap>
+							<S.ContentInput
+								type="password"
+								name="password"
+								minlength="8"
+								placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+								value={pw}
+								onChange={handlePassword}
+							/>
+							{/* <S.Check id="passwordCheck"></S.Check> */}
+						</S.ContentInputWrap>
+						<S.ErrorMessageWrap>
+							{!pwValid && pw.length > 0 && (
+								<div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+							)}
+						</S.ErrorMessageWrap>
+					</S.Content>
+
+					<S.Content>
+						<S.ContentTitle>비밀번호 확인</S.ContentTitle>
+						<S.ContentInputWrap>
+							<S.ContentInput type="password" id="passwordConfirm"></S.ContentInput>
+							{/* <S.Check id="passwordConfirmCheck"></S.Check> */}
+						</S.ContentInputWrap>
+					</S.Content>
+				</S.SignupContent>
+
+				<S.Button id="sign-up-button" onclick="handleSignUpButtonClick()" disabled={notAllow}>
+					가입하기
+				</S.Button>
+			</S.SignupContainer>
+
+			{/* <div class="sign-up-completion-modal-overlay">
+				<div class="sign-up-completion-modal">
+					<h2>가입 성공!</h2>
+					<p>AppsCheck 가입을 축하합니다!!</p>
+					<div class="sign-up-completion-modal-button-wrapper">
+						<button onclick="handleSignUpCompletionModalCloseButtonClick()">닫기</button>
+					</div>
+				</div>
+			</div> */}
+		</S.Container>
+	);
 }
--- src/pages/SignupPage/SignupPage.style.jsx
+++ src/pages/SignupPage/SignupPage.style.jsx
@@ -1,1 +1,107 @@
 import styled from "@emotion/styled";
+
+export const Container = styled.div`
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	width: 100%;
+	max-width: 500px;
+	display: flex;
+	padding: 0 20px;
+	left: 50%; // 가운데에 배치
+	transform: translate(-50%, 0);
+	flex-direction: column;
+	background-color: #f7f7f7;
+	overflow: hidden;
+	display: flex;
+	flex-direction: column;
+	margin: 0;
+`;
+
+export const SignupContainer = styled.div``;
+
+export const SignupTitle = styled.h1`
+	margin-top: 87px;
+	font-size: 26px;
+	font-weight: 700;
+	color: #262626;
+	display: flex;
+`;
+
+export const SignupContent = styled.div`
+	gap: 20px;
+	margin-bottom: 20px;
+`;
+
+export const Divider = styled.div`
+	display: flex;
+	gap: 20px;
+`;
+
+export const Content = styled.div`
+	margin-top: 26px;
+	flex: 1; //남는 공간을 차지하도록 함
+`;
+
+export const ContentTitle = styled.h3`
+	font-size: 12px;
+	font-weight: 600;
+	color: #262626;
+`;
+
+export const ContentInputWrap = styled.div`
+	display: flex;
+	border-radius: 8px;
+	padding: 16px;
+	margin-top: 8px;
+	background-color: #fff;
+	border: 1px solid #e2e0e0;
+	font-size: 20px;
+
+	// 포커스가 있는 경우의 스타일
+	&:focus-within {
+		border: 1px solid #2b80ff;
+	}
+`;
+
+export const ContentInput = styled.input`
+	width: 100%;
+	outline: none;
+	border: none;
+	height: 17px;
+	font-size: 14px;
+	font-weight: 400;
+
+	&::placeholder {
+		color: #dadada;
+	}
+`;
+// .sign-up-content > input {
+// 	border: 1px solid white;
+// 	border-radius: 5px;
+// 	height: 20px;
+// }
+
+export const ErrorMessageWrap = styled.div`
+	margin-top: 8px;
+	color: #ef0000;
+	font-size: 12px;
+`;
+
+export const Button = styled.button`
+	width: 100%;
+	height: 48px;
+	border: none;
+	font-weight: 700; // 700이 bold
+	background-color: #2b80ff;
+	border-radius: 64px;
+	color: white;
+	margin-bottom: 16px;
+	cursor: pointer;
+
+	// 버튼이 활성화되지 않았을때의 상태를 지정해줌
+	&:disabled {
+		background-color: #dadada;
+		color: #fff;
+	}
+`;
